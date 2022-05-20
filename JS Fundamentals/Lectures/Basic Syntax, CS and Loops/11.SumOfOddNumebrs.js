9a532bcbe74e685744f94d651d67f48a83546271
aleksapostolov <aleksandarvapostolov@gmail.com>
2022-05-20 13:14:26 +0300

    commit

--- JS Fundamentals/Lectures/Basic Syntax, CS and Loops/11.SumOfOddNumebrs.js
+++ JS Fundamentals/Lectures/Basic Syntax, CS and Loops/11.SumOfOddNumebrs.js
@@ -1,5 +1,29 @@
 function SumOfOddNumebrs(n){
 
-    (for i=n)
+    let sum = 0;
+    let counter = 0;
+    let i = 0;
+
+    while (i <= 100){
+    
+    
+
+        if(i % 2 != 0){
+            console.log(i)
+            counter++
+            sum+=i;
+        }
+           
+        
+        i++
+
+        if(counter === n){
+            break;
+        }
+
+
+    }
+    console.log(`Sum: ${sum}`);
+    
 }
 SumOfOddNumebrs(5);
