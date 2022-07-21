1ccbcacbaf28b545010b5cc50b606e044cc0661d
Aleks Apostolov <97387270+aleksapostolov@users.noreply.github.com>
2022-07-21 13:12:49 +0300

    Regex

--- JS Fundamentals/Lectures/09.Regex/02.MatchPhoneNumber.js
+++ JS Fundamentals/Lectures/09.Regex/02.MatchPhoneNumber.js
@@ -11,7 +11,7 @@ function matchPhoneNumber(data){
         match = pattern.exec(phoneNumbersList)
     }
 
-    console.log(result)
+    console.log(result.join(', '))
 
 }   
 matchPhoneNumber([359-2-222-2222, +359/2/222/2222, +359-2-222-222, +359-2-222-22222]);
