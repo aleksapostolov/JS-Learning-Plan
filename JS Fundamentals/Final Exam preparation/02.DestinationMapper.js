075ed1275f30aea4493c3bb31abc64a0eebe98a5
Aleks Apostolov <97387270+aleksapostolov@users.noreply.github.com>
2022-07-23 14:38:04 +0300

    exam prep

--- JS Fundamentals/Final Exam preparation/02.DestinationMapper.js
+++ JS Fundamentals/Final Exam preparation/02.DestinationMapper.js
@@ -1,5 +1,18 @@
 function destionationMapper(data){
 
+    let pattern = /([=|/])(?<destination>[A-Z]{1}[A-Za-z]{2,})\1/g;
+    let points = 0;
+    let valid;
+    let destinations = [];
+
+    while ((valid = pattern.exec(data))){
+        
+        points += valid.groups.destination.length;
+        destinations.push(valid.groups.destination)
+    }
+    
+    console.log(`Destinations: ${destinations.join(', ')}`)
+    console.log(`Travel Points: ${points}`)
 }
 destionationMapper("=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=");
-    destionationMapper("ThisIs some InvalidInput");
+destionationMapper("ThisIs some InvalidInput");
